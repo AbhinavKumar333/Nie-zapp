e6d2dfb365d3fab7b94389a244398a54b6d1a1f2
AbhinavKumar333 <abhinav333.ak@outlook.com>
2018-04-13 19:42:45 +0530

    Added firebase

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for, session
-
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://niezapp-ce5a9.firebaseio.com',None)
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template('index.html')	
-
+@app.route("/", methods=['POST', 'GET'])
+def main():	
+	if request.method == 'POST':
+		res = request.form
+		ip = firebase.post('/posts' + postId,res['postInput'])		
+		ret = firebase.get('/posts',None)
+		return render_template('index.html',d=ret)
+	ret = firebase.get('/posts',None)
+	return render_template('index.html',d=ret)
 
 if __name__ == "__main__":
 	app.run(debug = True, threaded = True)
